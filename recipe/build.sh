fcd59edf5fe02ddf2291275afa55154bd34a46d2
Mervin Fansler <mmfansler@gmail.com>
2024-07-27 01:03:01 +0200

    typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 ## manual patch
 ## NB: This should be removed in the next release!
 bzip2 -d inst/tests/tests.Rraw.bz2
-patch --verbose -V none inst/tests/tests.Rraw.bz2 < ${RECIPE_DIR}/patches/0001-no-warn-old-dates.patch
+patch --verbose -V none inst/tests/tests.Rraw < ${RECIPE_DIR}/patches/0001-no-warn-old-dates.patch
 bzip2 inst/tests/tests.Rraw
 
 export DISABLE_AUTOBREW=1
