3b7174344ef516f2d776f7dbede78d539c80351f
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-10-17 13:53:20 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,5 +6,5 @@ bzip2 -d inst\tests\tests.Rraw.bz2
 git apply --verbose < %RECIPE_DIR%\patches\0001-conditional-test-1623.patch
 bzip2 inst\tests\tests.Rraw
 
-"%R%" CMD INSTALL --build . %R_ARGS%
+"%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit /B 1
