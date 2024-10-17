60eb25ec45d9f35be3ee48fbe3c36ad662cbdc76
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-10-17 09:46:17 +0200

    git patch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 :: manual patch
 :: NB: This should be removed in the next release!
 bzip2 -d inst\tests\tests.Rraw.bz2
-patch --verbose -V none inst\tests\tests.Rraw < %RECIPE_DIR%\patches\0001-conditional-test-1623.patch
+git apply < %RECIPE_DIR%\patches\0001-conditional-test-1623.patch
 bzip2 inst\tests\tests.Rraw
 
 "%R%" CMD INSTALL --build . %R_ARGS%
