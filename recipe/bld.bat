a39ed3db1854f642e5d13f11d4a486ec46c84b6f
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-10-17 13:33:01 +0200

    verbose patching

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,9 @@
+@echo on
+
 :: manual patch
 :: NB: This should be removed in the next release!
 bzip2 -d inst\tests\tests.Rraw.bz2
-git apply < %RECIPE_DIR%\patches\0001-conditional-test-1623.patch
+git apply --verbose < %RECIPE_DIR%\patches\0001-conditional-test-1623.patch
 bzip2 inst\tests\tests.Rraw
 
 "%R%" CMD INSTALL --build . %R_ARGS%
