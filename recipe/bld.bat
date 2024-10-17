68e16873ecb8a96ab452457438064afaf8f58664
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-10-17 09:23:14 +0200

    manual patch on compressed test file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,8 @@
-"%R%" CMD INSTALL --build .
-IF %ERRORLEVEL% NEQ 0 exit 1
+:: manual patch
+:: NB: This should be removed in the next release!
+bzip2 -d inst\tests\tests.Rraw.bz2
+patch --verbose -V none inst\tests\tests.Rraw < %RECIPE_DIR%\patches\0001-conditional-test-1623.patch
+bzip2 inst\tests\tests.Rraw
+
+"%R%" CMD INSTALL --build . %R_ARGS%
+IF %ERRORLEVEL% NEQ 0 exit /B 1
